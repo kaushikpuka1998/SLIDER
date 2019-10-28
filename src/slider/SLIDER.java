75472618c384eb317b57fa936fd8f59a826c8dfa
kaushikpuka1998 <52675676+kaushikpuka1998@users.noreply.github.com>
2019-10-28 12:27:41 +0530

    Initial commit

---
+++ src/slider/SLIDER.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slider;
+
+/**
+ *
+ * @author KAUSHIK GHOSH
+ */
+public class SLIDER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame nj = new NewJFrame();
+        nj.setVisible(true);
+    }
+    
+}
